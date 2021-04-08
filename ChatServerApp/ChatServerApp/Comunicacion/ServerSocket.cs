c67e4ece882911a5e462f244a36373a78b0b8820
Franco Madariaga <frankurox@gmail.com>
2021-04-08 16:28:31 -0400

    Agrega Servidor base Socket

---
+++ ChatServerApp/ChatServerApp/Comunicacion/ServerSocket.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServerApp.Comunicacion
+{
+    public class ServerSocket
+    {
+        /*Atributo del puerto*/
+        private int puerto;
+
+        /*Se define como atributo variable de tipo Socket que sera el servidor*/
+        private Socket servidor;
+
+        /*Se define como atributo un socket que represente la comunicacion activa con el cliente*/
+        private Socket comCliente;
+
+        /*instancias que necesito para cuando se reciba el cliente
+         leer desde el cliente
+         y escribir al cliente*/
+        private StreamReader reader;
+        private StreamWriter writer;
+
+        /*Constructor que recibe el puerto*/
+        public ServerSocket(int puerto)
+        {
+            this.puerto = puerto;
+        }
+
+        /*Levanta la conexion*/
+        public Boolean Iniciar()
+        {
+            /*se hace boolean porque pude explotar*/
+            try
+            {
+                //1. Crear Socket
+                /*se crea instancia
+                 Que tipo de conexion de red soporta, de que tipo es el socket, protocolo*/
+                this.servidor = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                //2 Tomar control del puerto
+                /*recibe un EndPoint
+                 recibe IPAddress que reprecenta las direcciones desde las cual se podran conectar a este socket
+                 y recibe el puerto*/
+                /*si esta linea no pasa
+                 cae en excepsion como false y el inicio va a dar error*/
+                this.servidor.Bind(new IPEndPoint(IPAddress.Any, this.puerto));
+
+                //3. Definir cuantos clientes atenderé
+                /*cuando la aplicacion es concurrente tiene sentido poner 10*/
+                this.servidor.Listen(10);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                return false;
+            }
+
+        }
+        /*Obtiene el cliente*/
+        public bool ObtenerCliente()
+        {
+            try
+            {
+                /*recibo la comunicacion con cliente y lo guardo aca
+                 se queda esperando hasta que llegue un cliente*/
+                this.comCliente = this.servidor.Accept();
+
+                /*stream se crea a partir del socket
+                 La conexion entre cliente y servidor se llama comCliente de tipo socket
+                 la carretera para enviar los datos de un extremo a otro es el Stream
+                 en esa carretera necesito lo que manda la info de un extremo a otro
+                 estos se llaman StreamReader y StreamWriter*/
+                /*Stream(Socket)
+                 SW(Stream)
+                 SR(Stream)*/
+                Stream stream = new NetworkStream(this.comCliente);
+                /*stream se le pasa al Writer y Reader*/
+                this.writer = new StreamWriter(stream);
+                this.reader = new StreamReader(stream);
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                return false;
+            }
+
+        }
+        /*Permite mandar el mensaje al cliente*/
+        public bool Escribir(String mensaje)
+        {
+
+            try
+            {
+                /*Metodos para escribir mensajes al cliente*/
+                this.writer.WriteLine(mensaje);
+                this.writer.Flush();
+                return true;
+            }
+            catch (IOException ex)
+            {
+
+                return false;
+            }
+
+        }
+        /*Permite recibir valor del cliente*/
+        public String Leer()
+        {
+            try
+            {
+                /*metodo para leer mensajes del cliente*/
+                return this.reader.ReadLine().Trim();
+            }
+            catch (IOException ex)
+            {
+                //return null porque es string
+                return null;
+            }
+
+        }
+        /*Permite cerrar conexion del cliente*/
+        public void CerrarConexion()
+        {
+            this.comCliente.Close();
+
+        }
+    }
+}
---
+++ ChatServerApp/ChatServerApp/Program.cs
@@ -0,0 +1,67 @@
+﻿using ChatServerApp.Comunicacion;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*dentro del ConfiguratiionManager encontramos el AppSettings
+             es equivalente a la etiqueta de AppSetting
+             este atributo tiene como llaves las llaves que definí en AppConfig
+             puerto representa el texto que puse en la llave
+             da el valor 2000 pero en string, entonces se convierte en entero*/
+            int puerto = Int32.Parse(ConfigurationManager.AppSettings["puerto"]);
+
+            /*mensaje mostrado en consola
+             {} entre llaves va el puerto*/
+            Console.WriteLine("Iniciando Servidor en puerto {0}",puerto);
+
+            /*se levanta un objeto de tipo ServerSocket para poder levantar servidor en 
+             este puerto en particular*/
+            ServerSocket servidor = new ServerSocket(puerto);
+
+            /*si es capaz de tomar control del puerto
+             se obtienen los clientes*/
+            if (servidor.Iniciar())
+            {
+                //mientras espera el cliente
+                while (true)
+                {
+                    //esperando Cliente
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Esperando Clientes...");
+                    //cuando obtenga un cliente
+                    if (servidor.ObtenerCliente())
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("Conexion Establecida!");
+                        Console.WriteLine("S: Hola Mundo Cliente!");
+                        /*se le manda un hola mundo a cliente que se conecta
+                         y se cierra
+                         y va a hacer la repeticion infinita*/
+                        servidor.Escribir("Hola Mundo Cliente!");
+                        /*Leer mensaje desde el cliente*/
+                        String mensaje = servidor.Leer();
+                        Console.WriteLine("C:{0}", mensaje);
+                        servidor.CerrarConexion();
+                    }
+                }
+                
+            }
+            /*si no es capaz de tomar el control del puerto*/
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("No es posible iniciar servidor");
+                Console.ReadKey(); //esperando a que la persona presione una tecla
+            }
+        }
+    }
+}
